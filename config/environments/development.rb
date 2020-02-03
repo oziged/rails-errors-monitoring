f6b19f33630c6e3abe3de2032128e4f7c87c8c6f
oziged <oziged@gmail.com>
2020-02-03 13:58:14 +0200

    upd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  Raygun.setup do |config|
+    config.api_key = "SxnpCNVhk1BoNn9csQ3ZA"
+  end
 end
