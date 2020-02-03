d741738deea841094d4c8535da745a4e91eea8cc
oziged <oziged@gmail.com>
2020-02-03 15:14:52 +0200

    1

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 require 'rails/all'
 require 'raygun4ruby'
 require 'sentry-raven'
+require 'bugsnag'
 
 Raygun.setup do |config|
   config.api_key = "SxnpCNVhk1BoNn9csQ3ZA"
@@ -9,6 +10,10 @@ end
 
 Raven.configure do |config|
   config.dsn = 'https://e94e78e1bb3749ca93197088ceec9fd0:56190e3b7cc447cbaccc616dc72f150e@sentry.io/2208192'
+end
+
+Bugsnag.configure do |config|
+  config.app_version = '1.3.0'
 end
 
 # Require the gems listed in Gemfile, including any gems
