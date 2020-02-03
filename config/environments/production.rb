cb68cbd03895445d294e5caf3fc475b182aad55f
oziged <oziged@gmail.com>
2020-02-03 13:46:52 +0200

    add api key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  Raygun.setup do |config|
+    config.api_key = "SxnpCNVhk1BoNn9csQ3ZA"
+  end
 end
